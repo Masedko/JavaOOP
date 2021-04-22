664456285174d86e0982e934f2c2dc53f05b6157
Masedko <andruugladkuu@gmail.com>
2021-04-23 00:06:26 +0300

    Finally, Lab4

--- lab4/src/lab4/Boat.java
+++ lab4/src/lab4/Boat.java
@@ -1,4 +1,24 @@
 package lab4;
 
 public class Boat {
+    String name;
+    boolean isRowing;
+    char quality;
+    int maxNumberOfPassengers;
+    int cost;
+
+    Boat(String name, boolean isRowing, char quality,
+         int maxNumberOfPassengers, int cost){
+        this.name = name;
+        this.isRowing = isRowing;
+        this.quality = quality;
+        this.maxNumberOfPassengers = maxNumberOfPassengers;
+        this.cost = cost;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getMaxNumberOfPassengers(){
+        return maxNumberOfPassengers;
+    }
 }
--- lab4/src/lab4/Main.java
+++ lab4/src/lab4/Main.java
@@ -1,9 +1,43 @@
 package lab4;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Boat[] boats = new Boat[]{
+                new Boat("Malchezaar", true, 'C', 2, 200),
+                new Boat("Alexstraza", false, 'B', 5, 900),
+                new Boat("Reno", false, 'S', 20, 15000),
+        };
+        printArray(boats);
+        sortByName(boats);
+        printArray(boats);
+        sortByQuality(boats);
+        printArray(boats);
+    }
+    private static void sortByName(Boat[] arrayOfBoats){
+        Arrays.sort(arrayOfBoats, Comparator.comparing(Boat::getName));
+    }
+    private static void sortByQuality(Boat[] arrayOfBoats){
+        Arrays.sort(arrayOfBoats, Comparator.comparing(Boat::getMaxNumberOfPassengers).reversed());
+    }
+    private static void printArray(Boat[] arrayOfBoats){
+        System.out.println("|  Name of boat  | Is this boat rowing " +
+                "| Quality of boat | Max number of passengers |  Cost of boat  |");
+        for (Boat i : arrayOfBoats) {
+            System.out.printf("|%s|%s|%s|%s|%s|\n",
+                    centerString(16, i.name),
+                    centerString(21, Boolean.toString(i.isRowing)),
+                    centerString(17, Character.toString(i.quality)),
+                    centerString(26, Integer.toString(i.maxNumberOfPassengers)),
+                    centerString(16, Integer.toString(i.cost)));
+        }
+    }
+    public static String centerString (int width, String s) {
+        return String.format("%-" + width  + "s",
+               String.format("%" + (s.length() + (width - s.length()) / 2) + "s", s));
     }
 }
 
